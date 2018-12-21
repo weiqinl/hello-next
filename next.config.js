0b251d7770790ca41602a7ba24e56c42d890cf03
liuweiqin <liuweiqin@tron.network>
2018-12-21 14:58:42 +0800

    A: 增加localExport指令

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ module.exports = {
 			'/sp/hello-nextjs': { page: '/staticpost', query: { title: 'hello-nextjs'}},
 			'/sp/learn-nextjs': { page: '/staticpost', query: { title: 'learn-nextjs'}},
 			'/p/481': { page: '/post', query: { id: '481' } },
-			'/p/975': { page: '/post', query: { id: '975' } }
+			'/p/975': { page: '/post', query: { id: '975' } },
+			'/sp/exporting-pages': {page: '/staticpost', query: { title: 'Learn to Export HTML pages'}},
+			'/sp/exporting-pages1': {page: '/staticpost', query: { title: 'Learn to Export HTML pages1'}}
 		}
 	}
 }
